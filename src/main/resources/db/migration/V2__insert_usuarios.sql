97c3ba84182c6896776faa94cf97ca0be7b24f3f
amfimfodes@gmail.com <amfimfodes@gmail.com>
2024-11-26 12:41:11 -0600

    BD usuarios flyway

---
+++ src/main/resources/db/migration/V2__insert_usuarios.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.usuarios
+("name", last_name, email, "password", birth_date, active, updated_at, created_at)
+VALUES('Ivan', 'Rubio', 'ivan.rubio@trainibit.com', '123', '2000-06-08', true, '2024-11-26 11:10:51.055', '2024-11-26 11:10:51.058');
+INSERT INTO public.usuarios
+("name", last_name, email, "password", birth_date, active, updated_at, created_at)
+VALUES('Brandon', 'Cruz', 'brandon.cruz@trainibit.com', '123', '2002-09-14', true, '2024-11-26 11:10:51.055', '2024-11-26 11:10:51.058');
+INSERT INTO public.usuarios
+("name", last_name, email, "password", birth_date, active, updated_at, created_at)
+VALUES('Justin', 'Vazquez', 'justin.vazquez@trainibit.com', '123', '2002-04-24', true, '2024-11-26 11:10:51.055', '2024-11-26 11:10:51.058');
+INSERT INTO public.usuarios
+("name", last_name, email, "password", birth_date, active, updated_at, created_at)
+VALUES('Diego', 'Dominguez', 'diego.dominguez@trainibit.com', '123', '2002-09-09', true, '2024-11-26 11:10:51.055', '2024-11-26 11:10:51.058');
