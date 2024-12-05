6e466eb6ebe991d4a30422c52d71591a7ae66ce5
dinosoudi <90942593+dinosoudi@users.noreply.github.com>
2024-12-05 09:31:35 -0600

    cambios mapper

--- src/main/java/com/trainibit/usuarios/request/UsuarioRequest.java
+++ src/main/java/com/trainibit/usuarios/request/UsuarioRequest.java
@@ -1,9 +1,5 @@
 package com.trainibit.usuarios.request;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
